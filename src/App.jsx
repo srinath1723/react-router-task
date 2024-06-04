b6ac9af94e03ce500b62de44721a0b53a4b1a2bd
srinath1723 <srinathsp890.com>
2024-06-04 11:47:08 +0530

    image commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,7 @@ import Datascience from "./components/Datascience";
 import Cybersecurity from "./components/Cybersecurity";
 import Career from "./components/Career";
 import './components/Components.css';
-import "/src/cardimage/Analytical-Skills.png";
-import "/src/cardimage/Data-Visualization-Tools.jpg";
-import "/src/cardimage/Deep-Learning.jpg";
-import "/src/cardimage/Expressjs.png";
-import "/src/cardimage/image.png";
-import "/src/cardimage/Machine-Learning.jpg";
-import "/src/cardimage/MongoDB.png";
-import "/src/cardimage/MySQL.png";
-import "/src/cardimage/Networking.jpg";
-import "/src/cardimage/operating-system.jpg";
-import "/src/cardimage/PngItem_2069813.png";
-import "/src/cardimage/power-bi.webp";
-import "/src/cardimage/Programming-and-Scripting.jpeg";
-import "/src/cardimage/Project-Management.jpg";
-import "/src/cardimage/R-Programming.webp";
-import "/src/cardimage/React.png";
-import "/src/cardimage/Security-Tools-and-Technologies.jpg";
-import "/src/cardimage/Technical-Skills.webp";
+
 const AppLayout = ({ element }) => {
   return (
     <div>
